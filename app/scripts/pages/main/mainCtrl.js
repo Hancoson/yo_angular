a05c557ce1f304c1f46580c1fc47c0d92b76ef2f
Hancoson <hancoson@163.com>
2015-04-29 18:31:45 +0800

    edit app

--- app/scripts/pages/main/mainCtrl.js
+++ app/scripts/pages/main/mainCtrl.js
@@ -2,10 +2,14 @@
  * Created by Hancoson on 15-4-28.
  */
 angular.module('ngApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    //.controller('aboutCtrl', ['$scope', 'service', '$http'],function ($scope, service, $http) {
+    .controller('mainCtrl', function ($scope, $http) {
+        //var config = {params:{u_id: '1'}};
+        //var url = host.dev;
+        var url = 'http://192.168.8.23:8001/json/main/main.json';
+        $http.get(url).success(function (data) {
+            $scope.main = data;
+        })
+
+
+    });
