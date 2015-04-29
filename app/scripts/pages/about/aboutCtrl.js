3dd41ec3d92ca260792d344989c1282b8d46fe4f
Hancoson <hancoson@163.com>
2015-04-29 15:27:35 +0800

    add json $http

--- app/scripts/pages/about/aboutCtrl.js
+++ app/scripts/pages/about/aboutCtrl.js
@@ -2,10 +2,14 @@
  * Created by Hancoson on 15-4-28.
  */
 angular.module('ngApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('aboutCtrl', function ($scope, $http) {
+        console.log('aboutCtrl')
+        $scope.Datas = {};
+        $http.get("http://192.168.8.23:8001/json/main/about.json").success(function(data){
+            $scope.tableDate=data;
+            //$scope.msg
+            console.log(data)
+        })
+
+
+    });
